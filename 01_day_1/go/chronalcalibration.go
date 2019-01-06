1f230c86a3b672f17844043f7e3964343013b607
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2019-01-06 15:48:42 -0300

    REFACTOR: day 1 part 1 in go

--- 01_day_1/go/chronalcalibration.go
+++ 01_day_1/go/chronalcalibration.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
@@ -13,17 +14,22 @@ func checkForErrors(err error) {
 	}
 }
 
-func main() {
-	file, err := os.Open("input.txt")
-	checkForErrors(err)
-	defer file.Close()
-
+func readFrequencyFromFile(fileHandler io.Reader) int {
 	currentFrequency := 0
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(fileHandler)
 	for scanner.Scan() {
 		changeFrequency, _ := strconv.Atoi(scanner.Text())
 		currentFrequency += changeFrequency
 	}
+
+	return currentFrequency
+}
+
+func main() {
+	fileHandler, err := os.Open("input.txt")
+	checkForErrors(err)
+	defer fileHandler.Close()
+	currentFrequency := readFrequencyFromFile(fileHandler)
 	fmt.Println(currentFrequency)
 }
